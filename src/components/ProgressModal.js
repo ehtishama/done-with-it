578b2a294235ce66ba1445425137d1b11c322573
Ehtisham <ehtishamhassan9@gmail.com>
2022-04-11 14:17:19 +0500

    Add backend module

--- src/components/ProgressModal.js
+++ src/components/ProgressModal.js
@@ -1,19 +1,37 @@
-import {Modal, View, StyleSheet} from "react-native";
-import {Bar as ProgressBar} from "react-native-progress"
+import { Modal, View, StyleSheet } from "react-native";
+import { Bar as ProgressBar } from "react-native-progress";
 import colors from "../config/colors";
+import AnimatedLottieView from "lottie-react-native";
 
-export default function ProgressModal({progress, visible, onDone}) {
-    return <Modal visible={visible}>
-        <View style={styles.container}>
-            <ProgressBar progress={progress} width={200} animated color={colors.primary}/>
-        </View>
-    </Modal>
+export default function ProgressModal({ progress, visible, onDone }) {
+    return (
+        <Modal visible={visible}>
+            <View style={styles.container}>
+                <AnimatedLottieView
+                    autoPlay
+                    loop
+                    source={require("../../assets/animations/done.json")}
+                    style={styles.animation}
+                />
+                <ProgressBar
+                    progress={progress}
+                    width={200}
+                    animated
+                    color={colors.primary}
+                />
+            </View>
+        </Modal>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         justifyContent: "center",
         alignItems: "center",
-        flex: 1
-    }
-})
+        flex: 1,
+    },
+    animation: {
+        width: 150,
+        backgroundColor: "gray",
+    },
+});
--- src/screens/ListingEditScreen.js
+++ src/screens/ListingEditScreen.js
@@ -2,14 +2,14 @@ import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
-import {Formik} from "formik";
+import { Formik } from "formik";
 import ErrorText from "../components/ErrorText";
 import * as Yup from "yup";
 import AppPicker from "../components/AppPicker";
 import * as Permissions from "expo-permissions";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import ImageInputList from "../components/ImageInputList";
-import listingsApi from "../api/listings"
+import listingsApi from "../api/listings";
 import ProgressModal from "../components/ProgressModal";
 
 const validationSchema = Yup.object().shape({
@@ -19,27 +19,62 @@ const validationSchema = Yup.object().shape({
     category: Yup.object().required().nullable().label("Category"),
 });
 
-const categories = [{"id": 1, "label": "Furniture", "icon": "floor-lamp", "backgroundColor": "#fc5c65"}, {
-    "id": 2, "label": "Cars", "icon": "car", "backgroundColor": "#fd9644"
-}, {"id": 3, "label": "Cameras", "icon": "camera", "backgroundColor": "#fed330"}, {
-    "id": 4, "label": "Games", "icon": "cards", "backgroundColor": "#26de81"
-}, {"id": 5, "label": "Clothing", "icon": "shoe-heel", "backgroundColor": "#2bcbba"}, {
-    "id": 6, "label": "Sports", "icon": "basketball", "backgroundColor": "#45aaf2"
-}, {"id": 7, "label": "Media", "icon": "headphones", "backgroundColor": "#4b7bec"}, {
-    "id": 8, "label": "Books", "icon": "book-open-variant", "backgroundColor": "#9A67E2"
-}, {"id": 9, "label": "Other", "icon": "window-maximize", "backgroundColor": "#7C8CA1"}]
+const categories = [
+    {
+        id: 1,
+        label: "Furniture",
+        icon: "floor-lamp",
+        backgroundColor: "#fc5c65",
+    },
+    {
+        id: 2,
+        label: "Cars",
+        icon: "car",
+        backgroundColor: "#fd9644",
+    },
+    { id: 3, label: "Cameras", icon: "camera", backgroundColor: "#fed330" },
+    {
+        id: 4,
+        label: "Games",
+        icon: "cards",
+        backgroundColor: "#26de81",
+    },
+    { id: 5, label: "Clothing", icon: "shoe-heel", backgroundColor: "#2bcbba" },
+    {
+        id: 6,
+        label: "Sports",
+        icon: "basketball",
+        backgroundColor: "#45aaf2",
+    },
+    { id: 7, label: "Media", icon: "headphones", backgroundColor: "#4b7bec" },
+    {
+        id: 8,
+        label: "Books",
+        icon: "book-open-variant",
+        backgroundColor: "#9A67E2",
+    },
+    {
+        id: 9,
+        label: "Other",
+        icon: "window-maximize",
+        backgroundColor: "#7C8CA1",
+    },
+];
 
 export default function ListingEditScreen() {
     const [imageUris, setImageUris] = useState([]);
 
-    const [progress, setProgress] = useState(0)
+    const [progress, setProgress] = useState(0);
 
-    const [uploading, setUploading] = useState(false)
+    const [uploading, setUploading] = useState(false);
 
     const requestPermission = async () => {
-        const {granted} = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+        const { granted } = await Permissions.askAsync(
+            Permissions.MEDIA_LIBRARY
+        );
 
-        if (!granted) alert("You need to enable permissions to access this feature.");
+        if (!granted)
+            alert("You need to enable permissions to access this feature.");
     };
 
     const handleImageAdd = (newImageUri) => {
@@ -47,79 +82,89 @@ export default function ListingEditScreen() {
     };
 
     const handleSubmit = async (values) => {
-        setUploading(true)
+        setUploading(true);
 
         const result = await listingsApi.postListing(
-            {...values, images: imageUris},
+            { ...values, images: imageUris },
             setProgress
-        )
+        );
 
-        setUploading(false)
+        setUploading(false);
 
-        if (!result.ok) return alert("Upload failed")
+        if (!result.ok) return alert("Upload failed");
 
         // TODO:: reset form
-        alert("Successfully uploaded")
-
-    }
+        alert("Successfully uploaded");
+    };
 
     useEffect(() => {
         requestPermission();
     }, []);
 
-    return (<Screen>
-        <ProgressModal progress={progress} visible={uploading}/>
-        <ImageInputList imageUris={imageUris} onImageAdd={handleImageAdd}/>
+    return (
+        <Screen>
+            <ProgressModal progress={progress} visible={true} />
+            <ImageInputList imageUris={imageUris} onImageAdd={handleImageAdd} />
 
-        <Formik
-            initialValues={{
-                title: "", price: "", category: null, description: "",
-            }}
-            onSubmit={handleSubmit}
-            validationSchema={validationSchema}
-        >
-            {({
-                  handleChange, handleSubmit, handleBlur, errors, setFieldValue, values,
-              }) => (<>
-                <AppTextInput
-                    placeholder="Title"
-                    onChangeText={handleChange("title")}
-                    onBlur={handleBlur("title")}
-                />
-                <ErrorText>{errors.title}</ErrorText>
-                <AppTextInput
-                    placeholder="Price"
-                    onChangeText={handleChange("price")}
-                    maxLength={8}
-                    keyboardType={"decimal-pad"}
-                />
-                <ErrorText>{errors.price}</ErrorText>
-                <AppPicker
-                    iconName="apps"
-                    items={categories}
-                    placeholder={"Category"}
-                    selectedItem={values.category}
-                    onSelectItem={(item) => {
-                        console.log(item);
-                        console.log(values);
-                        setFieldValue("category", item);
-                    }}
-                />
-                <ErrorText>{errors.category}</ErrorText>
-                <AppTextInput
-                    placeholder="Description"
-                    onChangeText={handleChange("description")}
-                    multiline
-                    numberOfLines={2}
-                />
-                <ErrorText>{errors.description}</ErrorText>
-                <AppButton
-                    title={"Post"}
-                    color={colors.primary}
-                    onPress={handleSubmit}
-                />
-            </>)}
-        </Formik>
-
-    </Screen>);
+            <Formik
+                initialValues={{
+                    title: "",
+                    price: "",
+                    category: null,
+                    description: "",
+                }}
+                onSubmit={handleSubmit}
+                validationSchema={validationSchema}
+            >
+                {({
+                    handleChange,
+                    handleSubmit,
+                    handleBlur,
+                    errors,
+                    setFieldValue,
+                    values,
+                }) => (
+                    <>
+                        <AppTextInput
+                            placeholder="Title"
+                            onChangeText={handleChange("title")}
+                            onBlur={handleBlur("title")}
+                        />
+                        <ErrorText>{errors.title}</ErrorText>
+                        <AppTextInput
+                            placeholder="Price"
+                            onChangeText={handleChange("price")}
+                            maxLength={8}
+                            keyboardType={"decimal-pad"}
+                        />
+                        <ErrorText>{errors.price}</ErrorText>
+                        <AppPicker
+                            iconName="apps"
+                            items={categories}
+                            placeholder={"Category"}
+                            selectedItem={values.category}
+                            onSelectItem={(item) => {
+                                console.log(item);
+                                console.log(values);
+                                setFieldValue("category", item);
+                            }}
+                        />
+                        <ErrorText>{errors.category}</ErrorText>
+                        <AppTextInput
+                            placeholder="Description"
+                            onChangeText={handleChange("description")}
+                            multiline
+                            numberOfLines={2}
+                        />
+                        <ErrorText>{errors.description}</ErrorText>
+                        <AppButton
+                            title={"Post"}
+                            color={colors.primary}
+                            onPress={handleSubmit}
+                        />
+                    </>
+                )}
+            </Formik>
+        </Screen>
+    );
 }
