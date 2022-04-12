d860383dae9fa0a6f3931b7d869ef84b90b785b4
Ehtisham <ehtishamhassan9@gmail.com>
2022-04-12 12:26:28 +0500

    Add request transformer to include authToken with each request

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,5 +1,13 @@
-import {create} from "apisauce"
+import { create } from "apisauce";
+import authStorage from "../auth/storage";
 
 export const apiClient = create({
     baseURL: "http://192.168.43.163:9001/api/",
-})
+});
+
+apiClient.addAsyncRequestTransform(async (request) => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+
+    request.headers["x-auth-token"] = token;
+});
